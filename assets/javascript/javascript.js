62bdd1d9520b3909f81d6689e5049696957346a4
Rhino <ryanharrisfl@gmail.com>
2019-03-12 22:31:07 -0400

    bootstrap layout added and firebase database configured.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDH6XcSwGIuZG76IIBeENEf7pIME9yrv6g",
+    authDomain: "train-schedule-83a24.firebaseapp.com",
+    databaseURL: "https://train-schedule-83a24.firebaseio.com",
+    projectId: "train-schedule-83a24",
+    storageBucket: "train-schedule-83a24.appspot.com",
+    messagingSenderId: "82306266452"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
