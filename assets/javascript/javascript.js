854a36b7da4df1e831c2f5e028e56e7197634183
Rhino <ryanharrisfl@gmail.com>
2019-03-14 23:46:45 -0400

    calculations added for time remaining

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,20 +11,21 @@ var config = {
   firebase.initializeApp(config);
   var database = firebase.database();
   
-  //var train = "";
-  //var destination = "";
-  //var time = "";
-  //var monthlyminutes = 0;
-  //var minutes = 0;
+  var train = "";
+  var destination = "";
+  var time = 0;
+  var monthlyminutes = 0;
+  var minutes = 0;
+  
 
 $("#add-train-btn").on("click", function(event){
     event.preventDefault();
     alert("we clicked the button");
-    var train = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var time = moment($("#train-time").val().trim(), "HH:MM").format("HH:MM");
+    train = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    time = moment($("#train-time").val().trim(), "HH:mm").format("HH:mm");
     //var time = $("#train-time").val().trim();
-    var minutes = $("#frequency").val().trim();
+    minutes = $("#frequency").val().trim();
 
     var newTrain = {
         train: train,
@@ -34,11 +35,12 @@ $("#add-train-btn").on("click", function(event){
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     };
 
-    // Uploads employee data to the database
+  // Uploads employee data to the database
   database.ref().push(newTrain);
 
     console.log(newTrain.train);
     console.log(newTrain.destination);
+    console.log(newTrain.time);
     console.log(newTrain.time);
     console.log(newTrain.minutes);
 
@@ -54,15 +56,26 @@ $("#add-train-btn").on("click", function(event){
     // storing the snapshot.val() in a variable for convenience
     console.log(childSnapshot.val());
 
+    
+    
     trainName = childSnapshot.val().train;
     trainDest= childSnapshot.val().destination;
-    //trainTime = childSnapshot.val().time;
+    trainTime = childSnapshot.val().time;
     trainMin = childSnapshot.val().minutes;
+    trainRemainder = childSnapshot.val().minutesAway;
+    var freqMin = parseInt(trainMin);
+    var dConverted = moment(trainTime, "HH:mm").subtract(1, 'years');    
+    var trainMinLeft = moment(dConverted).format("HH:mm");
+    var timeConverted = moment(trainMinLeft, "HH:mm").subtract(1, 'years');
+    var timeDifference = moment().diff(moment(timeConverted), 'minutes');
+    var timeRemain = timeDifference % freqMin;
+    var minutesAway = freqMin - timeRemain;
+    var nextTrain = moment().add(minutesAway, 'minutes');
 
     // Console.loging the last user's data
     console.log(trainName);
     console.log(trainDest);
-    //console.log(trainTime);
+    console.log(trainTime);
     console.log(trainMin);
 
     //emptimePretty = moment.unix(sv.time).format("MM/DD/YYYY");
@@ -72,8 +85,8 @@ $("#add-train-btn").on("click", function(event){
     $("<td>").text(trainName),
     $("<td>").text(trainDest),
     $("<td>").text(trainMin),
-    //$("<td>").text(trainTime),
-    $("<td>").text(""),
+    $("<td>").text(moment(nextTrain, 'HH:mm').format("hh:mm a")),
+    $("<td>").text(minutesAway + ' Minutes away'),
     
 
   );
